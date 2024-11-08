395814c84783b406342f4f89825c3790343e4e3b
ana-caroliny <00001119570803sp@al.educacao.sp.gov.br>
2024-11-08 07:47:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'one ondirection',
+    'quantos albuns a boyband lancou? ',
+    '5 albuns '
+)
+
+criaCartao(
+    'supernatural',
+    'quantas vezes o dean morreu?',
+    '111 vezes'
+)
+
+criaCartao(
+    'guns n roses',
+    'quantas vezes o Steve Adler teve uma overdose ?',
+    '2 vezes'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
